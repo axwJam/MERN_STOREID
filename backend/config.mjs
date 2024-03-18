0274fc13c54ac1f4448e29f11f82893d3e779d1a
IrsanJam <irsandjamaludin@gmail.com>
2024-03-18 17:59:08 +0700

    remove export not used

--- backend/config.mjs
+++ backend/config.mjs
@@ -4,7 +4,6 @@ dotenv.config();
 
 export const urlPort = 5555;
 export const mongoDBURL = `mongodb://127.0.0.1:27017/store_id`;
-export const jwtSecret = process.env.JWT_SECRET;
 export const cloudName = process.env.CLOUD_NAME;
 export const cloudApiKey = process.env.CLOUD_API_KEY;
 export const cloudApiSecret = process.env.CLOUD_API_SECRET;
