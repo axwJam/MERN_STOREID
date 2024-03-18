f9e4ef49e14f3fd4890dbd21a6bf995fe2209c73
IrsanJam <irsandjamaludin@gmail.com>
2024-03-18 18:44:28 +0700

    add

--- backend/config.mjs
+++ backend/config.mjs
@@ -1,2 +1,3 @@
 export const urlPort = 5555;
+// export const mongoDBURL = `mongodb://127.0.0.1:27017/store_id`;
 export const mongoDBURL = `mongodb+srv://root:root@mern.n3yeiix.mongodb.net/store_id?retryWrites=true&w=majority&appName=mern`;
