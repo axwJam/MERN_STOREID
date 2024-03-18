7ed8205c14b19cfc76e3a998874c3ede6f1cc9c2
Irsan Jamaludin <114846911+IrsanJam@users.noreply.github.com>
2024-03-18 18:45:26 +0700

    Deploy (#3)
    * remove export not used
    
    * repair #2
    
    * repair #3
    
    * add

--- backend/config.mjs
+++ backend/config.mjs
@@ -1,10 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const urlPort = 5555;
-export const mongoDBURL = `mongodb://127.0.0.1:27017/store_id`;
-export const jwtSecret = process.env.JWT_SECRET;
-export const cloudName = process.env.CLOUD_NAME;
-export const cloudApiKey = process.env.CLOUD_API_KEY;
-export const cloudApiSecret = process.env.CLOUD_API_SECRET;
+// export const mongoDBURL = `mongodb://127.0.0.1:27017/store_id`;
+export const mongoDBURL = `mongodb+srv://root:root@mern.n3yeiix.mongodb.net/store_id?retryWrites=true&w=majority&appName=mern`;
