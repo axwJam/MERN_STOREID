022e7f8f8f7bf5a4d90e4afa873082982247aa18
IrsanJam <irsandjamaludin@gmail.com>
2024-03-18 18:43:22 +0700

    repair #3

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(
 app.use("/", [authModel, userModel, storeModel, productModel, cartModel, ordersModel, boxModel, paymentModel]);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to StoreId" });
+  res.send("Welcome to MERN Project");
 });
 
 mongoose
