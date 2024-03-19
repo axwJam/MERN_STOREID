3b70bbc71cc5eab29aa7f64b1720957919e983a3
Irsan Jamaludin <114846911+IrsanJam@users.noreply.github.com>
2024-03-19 09:09:10 +0700

    Deploy (#13)
    * feat #6
    
    * feat #7
    
    * feat #9
    
    * feat #10
    
    * feat #13

--- backend/cloudinary.mjs
+++ backend/cloudinary.mjs
@@ -1,10 +1,12 @@
 import cloudinary from "cloudinary";
-import { cloudName, cloudApiKey, cloudApiSecret } from "./config.mjs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: cloudName,
-  api_key: cloudApiKey,
-  api_secret: cloudApiSecret,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 export default cloudinary;
