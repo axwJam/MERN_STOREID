70b8c90bc01df9095b4266030df3e95e0a7eb69c
IrsanJam <irsandjamaludin@gmail.com>
2024-03-19 08:42:53 +0700

    feat #12

--- backend/cloudinary.mjs
+++ backend/cloudinary.mjs
@@ -1,8 +1,5 @@
 import cloudinary from "cloudinary";
-import dotenv from "dotenv";
 import { cloudName, cloudApiKey, cloudApiSecret } from "./config.mjs";
-
-dotenv.config();
 
 cloudinary.config({
   cloud_name: cloudName,
